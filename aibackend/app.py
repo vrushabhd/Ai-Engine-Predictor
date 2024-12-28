73b3d0965d34e5d650f9a4cbc4054d32f9350e8e
Vrushabh Dhond <vrushabhdhond1907@gmail.com>
2024-12-28 19:31:59 +0530

    run check

--- aibackend/app.py
+++ aibackend/app.py
@@ -2,11 +2,13 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS  # Correct import
 import joblib
 import pandas as pd
-
+import os
+print("Current working directory:", os.getcwd())
 app = Flask(__name__)
 CORS(app)
 # Load the model
 model = joblib.load("model.pkl")
+
 
 @app.route("/predict", methods=["POST"])
 def predict():
